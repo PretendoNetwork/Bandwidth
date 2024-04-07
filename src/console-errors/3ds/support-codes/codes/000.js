a263ed57738fb22d54aecb02c7ec20efceac6db8
Sarah <164569138+Yevnilc-S-C@users.noreply.github.com>
2024-04-07 16:21:17 -0300

    Update 000.js

--- src/console-errors/3ds/support-codes/codes/000.js
+++ src/console-errors/3ds/support-codes/codes/000.js
@@ -1,5 +1,5 @@
 const SupportCode = require('../../../common/support-code');
 
 module.exports = {
-	'0000': new SupportCode('missing name', 'Rverse and Juxtaposition are conflicting, resulting in this error\nthis error happens alongside 012-1511\nwhich has the same fix', 'An error has occurred.\nPlease try again later.\n\nIf the problem persists, please\nmake a note of the error code and\nvisit support.nintendo.com.', 'Uninstall rev.pem and reinstall nimbus', 'https://github.com/PretendoNetwork/nimbus/releases \ndownload the combined zip and extract to the root of your sd card.')
+	'0000': new SupportCode('Missing name', 'Rverse and Juxtaposition are conflicting, resulting in this error\nthis error happens alongside 012-1511\nwhich has the same fix', 'An error has occurred.\nPlease try again later.\n\nIf the problem persists, please\nmake a note of the error code and\nvisit support.nintendo.com.', 'Uninstall rev.pem and reinstall nimbus', 'https://github.com/PretendoNetwork/nimbus/releases \ndownload the combined zip and extract to the root of your sd card.')
 };
