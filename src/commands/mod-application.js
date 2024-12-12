1a6927faad660e21ea179cb25f1899d193f506d0
Alex Guerra <ajguerra2001@gmail.com>
2024-12-12 11:40:35 -0800

    Update slash command for mod application to accept new parameter determining the moderator role to apply to.

--- src/commands/mod-application.js
+++ src/commands/mod-application.js
@@ -7,6 +7,8 @@ const { modal: modApplicationModal } = require('../modals/mod-application');
  * @param {Discord.CommandInteraction} interaction
  */
 async function modApplicationHandler(interaction) {
+	modApplicationModal.setCustomId("mod-application-" + interaction.options.get('role').value);
+	
 	interaction.showModal(modApplicationModal, {
 		client: interaction.client,
 		interaction: interaction
@@ -16,11 +18,22 @@ async function modApplicationHandler(interaction) {
 const command = new SlashCommandBuilder()
 	.setDefaultMemberPermissions(Discord.PermissionFlagsBits.SendMessages)
 	.setName('mod-application')
-	.setDescription('Apply for a position as a moderator');
+	.setDescription('Apply for a position as a moderator.')
+	.addStringOption(option =>
+		option.setName('role')
+			.setDescription('Moderator type to apply to.')
+			.setRequired(true)
+			.addChoices(
+				{ name: 'Discord Moderator', value: 'discord' },
+				{ name: 'Discord VC Moderator', value: 'vc' },
+				{ name: 'Forum Moderator', value: 'forum' },
+				{ name: 'Network Moderator', value: 'network' },
+				{ name: 'Juxtaposition Moderator', value: 'juxt' }
+			));
 
 module.exports = {
 	name: command.name,
-	help: 'Displays a popup modal to apply for a moderator position.\n```\nUsage: /mod-application\n```',
+	help: 'Displays a popup modal to apply for a moderator position.\n```\nUsage: /mod-application role\n```',
 	handler: modApplicationHandler,
 	deploy: command.toJSON()
 };
