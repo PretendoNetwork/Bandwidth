19927b56419d3a4ee86a466f2d44e010941bcd80
ADev531 <adev531@outlook.com>
2026-01-01 08:18:28 +0900

    feat: modified error code regex patterns

--- src/utils/errorCode.js
+++ src/utils/errorCode.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js');
 const errors = require('@pretendonetwork/error-codes');
 
-const WIIU_SUPPORT_CODE_REGEX = /(\b1\d{2}-\d{4}\b)/gm;
-const THREE_DS_SUPPORT_CODE_REGEX = /(\b0\d{2}-\d{4}\b)/gm;
-// * There is probably a better way to do this regex
-const PRETENDO_SUPPORT_CODE_REGEX = /(\b678-\d{4}\b|\b598-\d{4}\b|\b727-\d{4}\b)/gm; // * 678 = Martini, 598 = Juxtaposition, 727 = PNID Account
+const WIIU_SUPPORT_CODE_REGEX = /(\b1\d{2}-(?:\d|X){4}\b)/gm;
+const THREE_DS_SUPPORT_CODE_REGEX = /(\b0\d{2}-(?:\d|X){4}\b)/gm;
+const PRETENDO_SUPPORT_CODE_REGEX = /(\b(?:678|598|727)-(?:\d|X){4}\b)/gm; // * 678 = Martini, 598 = Juxtaposition, 727 = PNID Account
 
 /**
  *
